87ae35dead87c707964838c6c0f12710e4fdd1a0
福島達也 <t.mugen282@gmail.com>
2016-06-22 02:25:21 +0900

    datepickerを修正

---
+++ todoru/CompleteViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CompleteViewController.swift
+//  todoru
+//
+//  Created by 福島達也 on 2016/06/21.
+//  Copyright © 2016年 Tatsuya Fukushima. All rights reserved.
+//
+
+import UIKit
+
+class CompleteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
